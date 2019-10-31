83250df1fe5d12dfaca3814bac2dc6049cb3a36e
andrebmsouza <andre.souza@barcelonagse.eu>
2019-10-31 10:33:14 +0100

    Organizes main file

--- gar_replicate.m
+++ gar_replicate.m
@@ -4,35 +4,19 @@
 % This script replicates the tables reported on the paper.
 % It is organized in sections, each of which runs a function that saves
 % a .mat file and outputs a status flag.
-%% Clearing
-clear all; clc
-%% Global Variable definitions and paths
-global HOME
-dr = dir('.');
-dr = {dr.name};
-if ~sum(contains(dr,'code'))==1
-    cd ../../ % Move to landing folder, if not there
-    HOME = cd ;
-else
-    HOME = cd;
-end
-addpath('code/')    
-addpath('code/utilities/')
+%% Run setup script
+setup;
 
-% This block for LASSO estimation of quantiles %%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% See also                                                                %
-addpath('code/utilities/SDPT3-4.0/')                               %
-addpath('code/utilities/SDPT3-4.0/Solver')                         %
-addpath('code/utilities/SDPT3-4.0/Solver/Mexfun')                  %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Data Cleaning
 % We leave 5\% of the sample for ``training'' forecast errors from
 % historical benchmarks.
 is = 0.2 ;
 os = 0.25;
-% Create data
+
+% Create data.mat file that will be used throughout
 create_data(is,os);
 
+% Define horizons of interest and coverage levels
 H  = [1 2 3 4];
 coverage = 0.95;
 
@@ -45,4 +29,4 @@ garch_oos( H , coverage);
 
 % Create tables for out-of-sample results
 create_tables(H , coverage) ;
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
